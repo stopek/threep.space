c06f32041866f3e25962a2a20bb781bab603c962
stopek <stopek.pawel@gmail.com>
2024-10-10 12:05:53 +0200

    feat: add long press click to add another stack to the url

--- src/components/Works/components/StackItem/index.tsx
+++ src/components/Works/components/StackItem/index.tsx
@@ -1,18 +1,51 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useContext, useRef, useState } from "react";
 import { Chip } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { useNavigate, useParams } from "react-router-dom";
-import { isCurrentStack } from "../../../../common/utils";
+import { isCurrentStack, stackArray } from "../../../../common/utils";
 import { fillRoute, paths } from "../../../../routing";
+import { SoundContext } from "../../../../hooks/useSound";
 
 export const StackItem = ({ name }: Type.IStackItem): ReactElement => {
 	const { t } = useTranslation();
 	const navigate = useNavigate();
 	const { filterId } = useParams();
+	const sound = useContext(SoundContext);
 
-	const goToStack = (): void => navigate(fillRoute(paths.PORTFOLIO_STACK, { stackName: name }));
+	const [timerId, setTimerId] = useState<NodeJS.Timeout | null>(null);
+	const isLongPress = useRef(false);
 
 	const existsInStackFilter = isCurrentStack(filterId, name);
+
+	const addStack = (): void =>
+		navigate(
+			fillRoute(paths.PORTFOLIO_STACK, {
+				stackName: Array.from(new Set(stackArray(filterId).concat(name))).join(","),
+			}),
+		);
+
+	const goToStack = (): void => {
+		if (!isLongPress.current) {
+			sound?.check();
+			navigate(fillRoute(paths.PORTFOLIO_STACK, { stackName: name }));
+		}
+	};
+
+	const handleMouseDown = (): void => {
+		isLongPress.current = false;
+		const timer = setTimeout(() => {
+			isLongPress.current = true;
+			addStack();
+		}, 500);
+		setTimerId(timer);
+	};
+
+	const handleMouseUp = (): void => {
+		if (timerId) {
+			clearTimeout(timerId);
+			setTimerId(null);
+		}
+	};
 
 	return (
 		<Chip
@@ -22,6 +55,8 @@ export const StackItem = ({ name }: Type.IStackItem): ReactElement => {
 			variant={existsInStackFilter ? "outlined" : "filled"}
 			color={existsInStackFilter ? "primary" : "default"}
 			clickable
+			onMouseDown={handleMouseDown}
+			onMouseUp={handleMouseUp}
 			onClick={goToStack}
 		/>
 	);
