a50ccfdd8b11055c13f21e47d1c18fc43b492b6f
stopek <stopek.pawel@gmail.com>
2024-10-10 06:32:31 +0200

    fix: clean helmet component

--- src/components/Helmet/index.tsx
+++ src/components/Helmet/index.tsx
@@ -3,7 +3,6 @@ import React, { ReactElement, ReactNode } from "react";
 import { useTranslation } from "react-i18next";
 import { helmetJsonLdProp } from "react-schemaorg";
 import { Person } from "schema-dts";
-import { useLocation } from "react-router-dom";
 
 export interface IHelmet {
 	disabled?: boolean;
@@ -23,7 +22,6 @@ const Helmet = ({
 	type = "article",
 }: IHelmet): ReactElement | null => {
 	const { t } = useTranslation();
-	const location = useLocation();
 
 	if (disabled) {
 		return null;
@@ -43,21 +41,6 @@ const Helmet = ({
 	const outputTitle = getText(titleArray.map(item => t(item, bindings)).join(" · "));
 	const outputDescription = getText(description ? t(description, bindings) : description);
 	const outputCanonical = canonical || window.location.pathname;
-
-	console.log({
-		outputTitle,
-		outputDescription,
-		location,
-		l: window.location,
-	});
-
-	// <meta name="author" content="Paweł Stopczyński" />
-	// <meta name="copyright" content="Paweł Stopczyński - All Rights Reserved" />
-	// <meta
-	// 	name="description"
-	// 	content="Hi! I’m Paweł, a fullstack developer based in Poland currently looking for a full-time remote job. Most of the projects in my portfolio are implemented end-to-end, but I also have experience working with international large teams."
-	// />
-	// <title>Paweł Stopczyński · Fullstack Developer</title>
 
 	return (
 		<HelmetAsync
