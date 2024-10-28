3724e238f89fb97df1ec986c69ae171c0d8924d3
stopek <stopek.pawel@gmail.com>
2024-10-28 21:40:30 +0100

    fix: hide cv

--- src/ui/CvButton/index.tsx
+++ src/ui/CvButton/index.tsx
@@ -6,7 +6,7 @@ import { StyledButton } from "./styled";
 const CvButton = (): ReactElement => {
 	const { t } = useTranslation();
 
-	return null;
+	return <span></span>;
 	return (
 		<StyledButton
 			variant="contained"
