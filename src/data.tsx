732f5a8cfc72b851196723d0aa52ca4dc6a7993e
stopek <stopek.pawel@gmail.com>
2024-10-29 14:39:01 +0100

    fix: remove photography

--- src/data.tsx
+++ src/data.tsx
@@ -134,16 +134,10 @@ export const hub_urls: TMenuItem[] = [
 		to: paths.PORTFOLIO,
 		Icon: BusinessCenterIcon,
 		label: "Portfolio",
-		margin: 3,
 	},
 	{
 		url: "https://www.facebook.com/pawel.stopek",
 		Icon: FacebookIcon,
 		label: "Facebook",
-	},
-	{
-		url: "https://znanyfotograf.com/pawel-stopczynski",
-		Icon: LinkedCameraIcon,
-		label: "Photography",
 	},
 ];
